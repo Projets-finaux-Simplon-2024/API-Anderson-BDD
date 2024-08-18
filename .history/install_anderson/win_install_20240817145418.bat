6b7cbb4d8cd281840cca8c63f394c5a0cf80ad8b
JeremyMikaleff <jeremy.mikaleff@gmail.com>
2024-08-18 16:56:53 +0200

    build(api-anderson-bdd): modification des executables pour construire le projet

--- .history/install_anderson/win_install_20240817145418.bat
+++ .history/install_anderson/win_install_20240817145418.bat
@@ -1,7 +1,6 @@
 @echo off
-
-REM Logout from any active Docker sessions to avoid potential pull issues
-docker logout ghcr.io
+REM Change to the directory where the docker-compose.yml file is located
+cd /d %~dp0
 
 REM Start Docker containers with Docker Compose
 docker-compose -p container_anderson_back_office up --build -d
