fb5397571a5fc829fcf40189ee5187cf4c28f055
JeremyMikaleff <jeremy.mikaleff@gmail.com>
2024-09-04 01:18:27 +0200

    tests(api-anderson-bdd): ajout des metriques et d'un network pour monitorer l'application

--- install_anderson/win_install.bat
+++ install_anderson/win_install.bat
@@ -3,6 +3,24 @@
 REM Logout from any active Docker sessions to avoid potential pull issues
 docker logout ghcr.io
 
+REM Check if the monitoring network exists
+docker network inspect monitoring_network >nul 2>&1
+
+REM If the network does not exist, create it
+IF %ERRORLEVEL% NEQ 0 (
+    echo Monitoring network not found. Creating the network...
+    FOR /F "tokens=*" %%i IN ('docker network create monitoring_network') DO SET NET_RESULT=%%i
+    IF NOT DEFINED NET_RESULT (
+        echo Failed to create the monitoring network.
+        echo Press any key to exit...
+        pause > nul
+        exit /b 1
+    )
+    echo Monitoring network created successfully with ID: %NET_RESULT%
+) ELSE (
+    echo Monitoring network already exists.
+)
+
 REM Start Docker containers with Docker Compose
 docker-compose -p container_anderson_back_office up --build -d
 
